7408835ff82b9c556f02e5addce0a2eb3ede649b
Eugenio <lugezz@gmail.com>
2023-02-25 16:08:17 -0300

    Minor change

---
+++ specific_info.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+
+from utils import get_html_content
+
+stud_html = get_html_content('samples/Stud_Book_Argentino.html', 'html.parser')
+soup = BeautifulSoup(stud_html, 'html.parser')
+
+full_table = soup.find('table', id="table-pedigree")
+print(full_table)
+print("=" * 100)
+
+print(full_table.tbody.find('tr').find('td').a.text)
+print(full_table.tbody.find_all('tr')[-1].find('td').a.text)
+
+print("=" * 100)
+
+temp_items = soup.find_all('p', class_="text-center")
+for temp_item in temp_items:
+    print(temp_item.text)
